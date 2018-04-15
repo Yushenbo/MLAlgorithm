00a4cfb75658ccd6cbd4b36a05551c4da793ab8f
nicholShen <shenbode@163.com>
2018-04-15 20:02:12 +0800

    [Bayes] Bayes Algoritm

--- NaiveBayes/bayes.py
+++ NaiveBayes/bayes.py
@@ -1,7 +1,7 @@
 #-*- coding:utf-8 -*-
 #########################################################################
 # File Name: bayes.py
-# Author: Shen Bo
+# Mail nichol_shen@yahoo.com
 # Created Time: Sun 08 Apr 2018 09:27:57 PM DST
 #########################################################################
 #!usr/bin/env python3
@@ -172,7 +172,7 @@ def bag_words2vec(vocab_list, input_set):
     
     return result
 
-def testing_naive_bayes():
+def testing_naive_bayesExp1():
     '''
     test Naive bayes algorithm
     return: no return
@@ -209,5 +209,192 @@ def testing_naive_bayes():
         p0v, p1v, p_abusive)))
 
 
+
+# Example 2 
+def text_parse(big_str):
+    '''
+    :param big_str: conbination string
+    :return: little latters
+    '''
+    import re
+    token_list = re.split(r'\W+', big_str)
+    if len(token_list) == 0:
+        print(token_list)
+    
+    return [tok.lower() for tok in token_list if len(tok) > 2]
+
+def spam_test():
+    '''
+    :return NULL
+    '''
+    doc_list = []
+    class_list = []
+    full_text = []
+    for i in range(1, 26):
+        try:
+            words = text_parse(open('./email/spam/{}.txt'.format(i)).read())
+        except:
+            words = text_parse(open('./email/spam/{}.txt'.format(i),
+            encoding='Windows 1252').read())
+
+        doc_list.append(words)
+        full_text.extend(words)
+        class_list.append(1)
+
+        try:
+            # Add to trash mails
+            words = text_parse(open('./email/ham/{}.txt'.format(i)).read())
+        except:
+            words = text_parse(open('./email/ham/{}.txt'.format(i),
+                encoding='Windows 1252').read())
+        doc_list.append(words)
+        full_text.extend(words)
+        class_list.append(0)
+
+    #Create vocabulary list
+    vocab_list = create_vocab_list(doc_list)
+    
+    training_set = list(range(50))
+    test_set = []
+    for i in range(10):
+        rand_index = int(np.random.uniform(0, len(training_set)))
+        test_set.append(training_set[rand_index])
+        del training_set[rand_index]
+
+    training_mat = []
+    training_class = []
+    for doc_index in training_set:
+        training_mat.append(set_of_words2vec(vocab_list, doc_list[doc_index]))
+        training_class.append(class_list[doc_index])
+
+    p0v, p1v, p_spam = train_naive_bayes(np.array(training_mat),
+            np.array(training_class))
+
+
+    #starting test
+    error_count =  0
+    for doc_index in test_set:
+        word_vec = set_of_words2vec(vocab_list, doc_list[doc_index])
+        if classify_naive_bayes(
+                np.array(word_vec),
+                p0v,
+                p1v,
+                p_spam
+                ):
+            error_count += 1
+            print('the error rate is {}'.format(
+                error_count / len(test_set)))
+
+# Example 3
+def clc_most_freq(vocab_list, full_text):
+    #Rss source classify & frequency clear
+    from  operator import itemgetter
+    freq_dict = {}
+    for token in vocab_list:
+        fre_dic[token] = full_text.count(token)
+    sorted_freq = sorted(fre_dic.items(), key=itemgetter(1), reverse=True)
+    return sorted_freq[0:30]
+
+def local_words(feed1, feed0):
+    doc_list = []
+    class_list = []
+    funll_text = []
+    # Find out the minist one
+    min_len = min(len(feed0), len(feed1))
+    for i in range(min_len):
+        #Class 1
+        word_list = text_parse(feed1['entries'][i]['summary'])
+        doct_list.append(word_list)
+        funll_text.extend(word_list)
+        class_list.append(1)
+
+        #Class 0
+        word_list = text_parse(feed0['entries'][i]['summary'])
+        doc_list.append(word_list)
+        full_text.extend(word_list)
+        class_list.append(0)
+
+    vocab_list = create_vocab_list(doc_list)
+    #Remove high frequency words
+    top30words = calc_mos_freq(vocab_list, full_text)
+    for pair in top30words:
+        if pair[0] in vocab_list:
+            vocab_list.remove(pair[0])
+
+    #Get training data
+    training_set = list(range(2 * min_len))
+    test_set = []
+    for i in range(20):
+        rand_index = int(np.random.uniform(0, len(training_set)))
+        test_set.append(training_set[rand_index])
+        del training_set[rand_index]
+
+    #Conver to Vector
+    training_mat = []
+    training_class = []
+    for doc_index in training_set:
+        training_mat.append(bag_words2vec(vocab_list, doc_list[doc_index]))
+        training_class.append(class_list[doc_index])
+
+    p0v, p1v, p_spam = train_naive_bayes(np.array(training_mat),
+            np.array(training_class))
+
+    error_count = 0
+    for doc_index in test_set:
+        word_vec = bag_word2vec(covab_list, doc_list[doc_index])
+        if classify_naive_bayes(
+                np.array(word_vec),
+                p0v,
+                p1v,
+                p_spam
+                ) != class_list[doc_index]:
+            error_count += 1
+
+    print('The error rate is {}'.format(error_count / len(test_set)))
+    return vocab_list, p0v, p1v
+
+def test_rss():
+    import feedparser
+    ny = feedparser.parse('http://shanghai.craigslist.org/stp')
+    sf = feedparser.parse('http://beijing.craigslist.org/stp')
+    vocab_list, p_sf, p_nf = local_words(ny, sf)
+    vocab_list, p_sf, p_nf = local_words(ny, sf)
+
+
+def get_top_words():
+    import feedparser
+    ny = feedparser.parse('https://shanghai.craigslist.com.cn/search/stp')
+    sf = feedparser.parse('https://beijing.craigslist.com.cn/search/stp')
+    test = feedparse.parse('http://blog.csdn.net/lanchunhui/rss/list')
+    print('======>', test)
+    vocab_list, p_sf, p_nf = local_words(ny, sf)
+    
+    top_ny = []
+    top_sf = []
+
+    for i in range(len(p_sf)):
+        if p_sf[i] > -6.0:
+            top_sf.append((vocab_list[i], p_sf[i]))
+        if p_ny[i] > -6.0:
+            top_ny.append((vocab_list[i], p_ny[i]))
+
+    sorted_sf = sorted(top_sf, key=lambda: pair[1], reverse=True)
+    sorted_ny = sorted(top_ny, key=lambda: pair[1], reverse=True)
+
+    print('\n','-'*80,'this is SF','-'*80, '\n')
+    for item in sorted_sf:
+        print(item[0])
+    print('\n','-'*80,'this is NY','-'*80, '\n')
+    for item in sorted_ny:
+        print(item[0])
+
+
 if __name__ == '__main__':
-    testing_naive_bayes()
+    print('\n','='*80,'this is Examp1','='*80, '\n')
+    testing_naive_bayesExp1()
+    print('\n','='*80,'this is Examp2','='*80, '\n')
+    spam_test()
+    print('\n','='*80,'this is testRss','='*80, '\n')
+    #test_rss()
+    print('\n','='*80,'this is SF','='*80, '\n')
+    #get_top_words()
